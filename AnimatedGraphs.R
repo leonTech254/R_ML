2b8d2a30c9c34d1ba03e1abbd2c182d61881afe5
Martin Wambugu <martinleontech23@gmail.com>
2024-12-02 17:03:43 +0300

    ---Animated visualize graphs

--- AnimatedGraphs.R
+++ AnimatedGraphs.R
@@ -1,6 +1,9 @@
+
+
 anim_hist <- ggplot(mtcars, aes(x=mpg)) + 
   geom_histogram(binwidth=5) + 
   xlab("Miles Per Gallon") + ylab("Frequency") + 
   transition_states(cyl, transition_length = 2, state_length = 1) 
 
 anim_save("animated_hist.gif", anim_hist)
+
